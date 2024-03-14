ab3fc5c2729acab6719974a56b8d59fde5cc1cdd
Bruno Pascutti <brupascutti@gmail.com>
2024-03-14 09:35:29 -0300

    tratanto erro com ex personalizada

--- PrimeiroProjC#/Program.cs
+++ PrimeiroProjC#/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using PrimeiroProjC_;
+using PrimeiroProjC_.tabuleiro;
 using PrimeiroProjC_.xadrez;
 using tabuleiro;
 using xadrez;
@@ -9,13 +10,20 @@ namespace PrimeiroProjC {
 {
     static void Main(string[] args)
     {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
     }
